facd0cdc828d22a8302482492ac4bbc8f8ab247c
bb420f@gmail.com <bb420f@gmail.com>
2023-06-17 19:17:54 +0500

    hogya hn

--- js/script.js
+++ js/script.js
@@ -1,21 +1,16 @@
 
 
 $(document).ready(function () {
-  // $(".menu_bar").click(function () {
-  //   $(".nav_links").toggleClass('active');
-  //   $('body').toggleClass('active');
-  //   $(this).find('.ti').toggleClass("ti-x");
-  // });
-  // $(".ti-caret-down").click(function () {
-  //   $(".cate_bottom").slideToggle();
-  //   $(this).toggleClass("ti-caret-down").toggleClass('ti-caret-up')
-  // });
 
-  // $('.modal').modal('show');
+
+  $(function () {
+    $('.lazy').Lazy();
+  });
+  
   $('.search_icon').click(function () {
     $('.header_search').toggleClass('active');
     $('.search_icon .ti').toggleClass("ti-pointer-search , ti-x");
-    // $('.nav_links').toggleClass('active')
+
   });
 });
 $('.menu').click(function () {
@@ -26,56 +21,15 @@ $('.menu').click(function () {
 
 
 
+
 $('.search_menu_icon').click(function () {
   $(".search_bottom").toggleClass('Active');
   $(".menu_search").toggleClass('Active');
 
   $('.search_menu_icon .ti').toggleClass("ti-pointer-search , ti-x");
 
-
 });
 
-
-let prevScrollPos = window.pageYOffset;
-
-window.onscroll = function () {
-  let currentScrollPos = window.pageYOffset;
-  // console.log("Scroll position:", currentScrollPos);
-
-  if (prevScrollPos > currentScrollPos) {
-    // User is scrolling up
-    document.querySelector("header").classList.add("visible");
-  } else {
-    // User is scrolling down
-    document.querySelector("header").classList.remove("visible");
-  }
-
-  prevScrollPos = currentScrollPos;
-};
-
-
-// var swiper = new Swiper(".store_swiper", {
-//   slidesPerView: "auto",
-//   grid: {
-//     rows: 2,
-//   },
-//   spaceBetween: 30,
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-// });
-
-
-// Show the loading screen
-function showLoadingScreen() {
-  $('#loading-screen').fadeIn();
-}
-
-// Hide the loading screen
-function hideLoadingScreen() {
-  $('#loading-screen').fadeOut();
-}
 
 $(".filter_").click(function (e) {
   e.preventDefault()
@@ -90,21 +44,18 @@ $('.filter_').click(function () {
   $(this).addClass('active').siblings().removeClass('active');
 });
 
-function handleSearchInput() {
-  var searchInput = $('#searchInput').val().toLowerCase();
 
-  // Filter the search result elements based on the input
-  $('.populor_card_side').each(function () {
-    var title = $(this).find('h4').text().toLowerCase();
-    if ($('#searchInput').val() !== '' && title.indexOf(searchInput) > -1) {
-      $(this).show();
-    }
-    else {
-      $(this).hide();
-    }
-  });
-}
 
+
+var container = $(window);
+var scrollSpeed = 10; // Adjust the scroll speed as needed
+
+container.on("wheel", function (event) {
+  event.preventDefault();
+  var wheelDelta = event.originalEvent.deltaY;
+
+  container.scrollLeft(container.scrollLeft() + wheelDelta * scrollSpeed);
+});
 
 
 
@@ -121,18 +72,37 @@ $('#searchInput').keyup(function () {
   }
 });
 
-var container = $(".hero_main");
-var scrollSpeed = 10; // Adjust the scroll speed as needed
+var swiper = new Swiper(".p_cate_main", {
+  loop: true,
+  slidesPerView: "auto",
+  speed: 500, 
+  spaceBetween: 30,
+  pagination: {
+    clickable: true,
+    el: ".Popular_Category .main_heading .swiper-pagination",
+  },
 
-container.on("wheel", function (event) {
-  event.preventDefault();
-  var wheelDelta = event.originalEvent.deltaY;
+});
+$(".filter_blog").click(function (e) { 
+  e.preventDefault();
+  $(".filter_blog").toggleClass("active");
+  var attr =   $(this).attr("data-type") ;
 
-  container.scrollLeft(container.scrollLeft() + wheelDelta * scrollSpeed);
+$(`.side_bar_post_main[data-type="${attr}"]`).addClass("active").siblings().removeClass("active");
 });
 
-
-$(function () {
-  $('.lazy').Lazy();
+var swiper = new Swiper(".blog_main", {
+  loop: true,
+  slidesPerView: "auto",
+  speed: 500, 
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
+
 
